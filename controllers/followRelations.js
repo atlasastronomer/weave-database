16a0bb01bd4dc8ea7014f390a87bb6ca6624c13c
Atlas Astronomer <atlasastronomer@gmail.com>
2025-07-11 17:39:56 -0400

    Fixed follow relations to prevent duplicate follows.

--- controllers/followRelations.js
+++ controllers/followRelations.js
@@ -15,76 +15,82 @@ const getTokenFrom = (req) => {
 
 followsRouter.get('/:id', async (req, res) => {
   const username = req.params.id
-  const user = await User.findOne({username: username})
+  const user = await User.findOne({ username })
 
-  const followRelations = await FollowRelations.findOne({user: user._id})
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const followRelations = await FollowRelations.findOne({ user: user._id })
 
   if (!followRelations) {
-    return res.status(404).json({message: 'User has no followers & is not following anyone.'})
+    return res.json({ followers: [], following: [] })
   }
 
   return res.json({followers: followRelations.followers, following: followRelations.following})
 })
 
 followsRouter.post('/:id', async (req, res) => {
-  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+  try {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
 
-  if (!decodedToken.id) {
-    return res.status(401).json({ error: 'token invalid' })
+    if (!decodedToken.id) {
+      return res.status(401).json({error: 'token invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const targetUsername = req.params.id
+    const targetUser = await User.findOne({username: targetUsername})
+
+    if (!user) {
+      return res.status(404).json({message: 'User not found'})
+    }
+
+    if (!targetUser) {
+      return res.status(404).json({ message: 'Target user not found'})
+    }
+
+    if (user.id === targetUser.id) {
+      return res.status(400).json({message: 'Cannot follow yourself'})
+    }
+
+    const userRelations = await FollowRelations.findOne({user: user._id}) ||
+      new FollowRelations({user: user._id, followers: [], following: []})
+
+    const targetRelations = await FollowRelations.findOne({user: targetUser._id}) ||
+      new FollowRelations({user: targetUser._id, followers: [], following: []})
+
+    const isFollowing = userRelations.following.some((id) => id.equals(targetUser._id))
+
+    if (isFollowing) {
+      userRelations.following = userRelations.following.filter((id) => !id.equals(targetUser._id))
+      targetRelations.followers = targetRelations.followers.filter((id) => !id.equals(user._id))
+    }
+    else {
+      if (!userRelations.following.some((id) => id.equals(targetUser._id))) {
+        userRelations.following.push(targetUser._id)
+      }
+      if (!targetRelations.followers.some((id) => id.equals(user._id))) {
+        targetRelations.followers.push(user._id)
+      }
+    }
+
+    await userRelations.save()
+    await targetRelations.save()
+
+    user.followRelations = userRelations
+    targetUser.followRelations = targetRelations
+
+    await user.save()
+    await targetUser.save()
+
+    return res.status(200).json({following: !isFollowing})
   }
-
-  const user = await User.findById(decodedToken.id)
-  const targetUsername = req.params.id
-  const targetUser = await User.findOne({username: targetUsername})
-
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' })
+  catch (error) {
+    console.error(error)
+    return res.status(500).json({error: 'Something went wrong'})
   }
-
-  if (!targetUser) {
-    return res.status(404).json({ message: 'Target user not found' })
-  }
-
-  if (user.id === targetUser.id) {
-    return res.status(400).json({ message: 'Cannot follow yourself' })
-  }
-
-  const userRelations = await FollowRelations.findOne({ user: user._id }).populate('followers').populate('following') 
-  || new FollowRelations({
-    user: user._id,
-    followers: [],
-    following: [],
-  })
-
-  const targetRelations = await FollowRelations.findOne({ user: targetUser._id }).populate('followers').populate('following')
-  || new FollowRelations({
-    user: targetUser._id,
-    followers: [],
-    following: [],
-  })
-
-
-  const isFollowing = userRelations.following.some(id => id.equals(targetUser._id))
-  
-  if (isFollowing) {
-    userRelations.following = userRelations.following.filter(id => !id.equals(targetUser._id))
-    targetRelations.followers = targetRelations.followers.filter(id => !id.equals(user._id))
-  }
-  else {
-    userRelations.following.push(targetUser._id)
-    targetRelations.followers.push(user._id)
-  }
-
-  await userRelations.save()
-  await targetRelations.save()
-  
-  user.followRelations = userRelations
-  targetUser.followRelations = targetRelations
-
-  await user.save()
-  await targetUser.save()
-
-  res.status(200).json({ following: !isFollowing })
 })
 
 module.exports = followsRouter
